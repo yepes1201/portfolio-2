ae3de05aebdac579bfbced398d6bfc59b4784835
Daniel Yepes <yepes1201@gmail.com>
2025-01-15 20:49:07 -0500

    styles: add NeueMachina font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: ["selector", "[data-theme='dark']"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        neue: ["Neue Machine", "sans-serif"],
+      },
+      colors: {
+        "primary-1": "#151515",
+        "primary-2": "#838089",
+        "primary-3": "#F0F0F0",
+        "primary-4": "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
