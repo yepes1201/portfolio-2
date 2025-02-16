7cb974a929f639de2cadedfa66c103504435f989
Daniel Yepes <yepes1201@gmail.com>
2025-02-16 14:39:53 -0500

    feat: implement resend

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  adapter: node({
+    mode: "standalone",
+  }),
 });
---
+++ src/actions/index.ts
@@ -0,0 +1,38 @@
+// Dependencies
+import { ActionError, defineAction } from "astro:actions";
+import { z } from "astro:schema";
+
+// Resend
+import { Resend } from "resend";
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const server = {
+  send: defineAction({
+    accept: "form",
+    input: z.object({
+      name: z.string(),
+      email: z.string(),
+      subject: z.string(),
+      message: z.string(),
+    }),
+    handler: async ({ email, name, message, subject }) => {
+      const { data, error } = await resend.emails.send({
+        from: `${name} <website@danielyepes.com>`,
+        to: ["yepes1201@gmail.com"],
+        subject: subject,
+        html: `<section><p>${message}</p><br/><strong>${name}<br/>${email}</strong></section>`,
+      });
+
+      if (error) {
+        console.log(error);
+        throw new ActionError({
+          code: "BAD_REQUEST",
+          message: error.message,
+        });
+      }
+
+      return data;
+    },
+  }),
+};
