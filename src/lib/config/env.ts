3cd01b33702765d0ebc32e914c74c382bfd9b7a6
Daniel Yepes <yepes1201@gmail.com>
2025-03-10 21:01:57 -0500

    docs: google indexing

--- src/lib/config/env.ts
+++ src/lib/config/env.ts
@@ -4,4 +4,5 @@ export const ENV = {
   CMS_ITEMS_URL: import.meta.env.CMS_ITEMS_URL,
   CMS_ASSETS_URL: import.meta.env.CMS_ASSETS_URL,
   CMS_ACCESS_TOKEN: import.meta.env.CMS_ACCESS_TOKEN,
+  GOOGLE_SITE_VERIFICATION: import.meta.env.GOOGLE_SITE_VERIFICATION,
 };
